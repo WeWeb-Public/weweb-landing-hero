8cbf594f72588bd615fdda1635a033daadbb193b
borisAtWeweb <boris@weweb.io>
2020-06-17 18:43:55 +0200

    add stylelint

--- src/bubble.vue
+++ src/bubble.vue
@@ -1,22 +1,6 @@
 <template>
-    <svg
-        :width="124 * scale"
-        :height="124 * scale"
-        viewBox="0 0 132 132"
-        fill="none"
-        class="bubble"
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <rect
-            x="4"
-            y="4"
-            width="122"
-            height="122"
-            rx="30"
-            fill="none"
-            :stroke="color"
-            :stroke-width="Math.ceil(4 / scale)"
-        ></rect>
+    <svg :width="124 * scale" :height="124 * scale" viewBox="0 0 132 132" fill="none" class="bubble" xmlns="http://www.w3.org/2000/svg">
+        <rect x="4" y="4" width="122" height="122" rx="30" fill="none" :stroke="color" :stroke-width="Math.ceil(4 / scale)"></rect>
     </svg>
 </template>
 <script>
@@ -24,7 +8,7 @@ export default {
     props: {
         scale: {
             type: Number,
-            default: 1
+            default: 20
         },
         color: {
             type: String,
@@ -35,7 +19,7 @@ export default {
 </script>
 <style>
 .bubble {
-    position: absolute;
-    transition: all 3000ms ease-in-out;
+  position: absolute;
+  transition: all 3000ms ease-in-out;
 }
 </style>
