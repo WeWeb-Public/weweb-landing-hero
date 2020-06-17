703eb968c4204c3e692d3ed620e58817fa7c3307
borisAtWeweb <boris@weweb.io>
2020-06-17 19:27:30 +0200

    fix css

--- src/bubble.vue
+++ src/bubble.vue
@@ -1,22 +1,6 @@
 <template>
-  <svg
-    :width="124 * scale"
-    :height="124 * scale"
-    viewBox="0 0 132 132"
-    fill="none"
-    class="bubble"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <rect
-      x="4"
-      y="4"
-      width="122"
-      height="122"
-      rx="30"
-      fill="none"
-      :stroke="color"
-      :stroke-width="Math.ceil(4 / scale)"
-    ></rect>
+  <svg :width="124 * scale" :height="124 * scale" viewBox="0 0 132 132" fill="none" class="bubble" xmlns="http://www.w3.org/2000/svg">
+    <rect x="4" y="4" width="122" height="122" rx="30" fill="none" :stroke="color" :stroke-width="Math.ceil(4 / scale)"></rect>
   </svg>
 </template>
 <script>
@@ -24,13 +8,13 @@ export default {
   props: {
     scale: {
       type: Number,
-      default: 20,
+      default: 20
     },
     color: {
       type: String,
-      default: '#000000',
-    },
-  },
+      default: '#000000'
+    }
+  }
 };
 </script>
 <style>
